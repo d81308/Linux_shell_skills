fd3cd9bca00d3e7c5e8c2a4f956d414553a88343
Neil Wu <114141600+d81308@users.noreply.github.com>
2023-02-02 15:08:11 +0800

    Add files via upload

---
+++ mini_program/ETL_function/T_str-space_reduce.sh
@@ -0,0 +1,4 @@
+## demo the ps information
+words="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.        1"
+
+echo $words | tr -s " "
