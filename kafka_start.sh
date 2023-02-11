55939105221210595dd9b77ecdd13cfbe892c39d
Neil Wu <114141600+d81308@users.noreply.github.com>
2023-02-11 17:01:05 +0800

    Add files via upload

---
+++ kafka_start.sh
@@ -0,0 +1,11 @@
+## create topic
+#/Users/jianlin/kafka_2.13-3.4.0/bin/kafka-topics.sh --create --topic news --bootstrap-server localhost:9092
+
+
+## edit messenger into topic
+#/Users/jianlin/kafka_2.13-3.4.0/bin/kafka-console-producer.sh --topic news --bootstrap-server localhost:9092
+
+
+## read messenger from topic
+#/Users/jianlin/kafka_2.13-3.4.0/bin/kafka-console-consumer.sh --topic news --from-beginner --bootstrap-server localhost:9092
+
